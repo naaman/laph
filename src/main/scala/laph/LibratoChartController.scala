67bc4aa10e093a2d4221dea06bfc5611bf5f56d4
Naaman Newbold <naaman@heroku.com>
2013-04-09 14:57:30 -0700

    Add titles to charts

--- src/main/scala/laph/LibratoChartController.scala
+++ src/main/scala/laph/LibratoChartController.scala
@@ -16,16 +16,10 @@ class LibratoChartController extends Controller {
         (for {
           chartId <- request.routeParams.get("chartId").map(_.toLong)
         } yield {
-          val instrumentFuture = Librato(u, p).instrument(chartId)
-          val chartFuture      = LibratoChart.createChart(LibratoChartView(u, p, chartId))
-
-          for {
-            instrument <- instrumentFuture
-            chart      <- chartFuture
-          } yield {
-            render.body(chart)
-              .header("Content-Type", "image/png")
-              .header("Chart-Name", instrument.name)
+          Librato(u, p).instrument(chartId).flatMap { instrument =>
+            LibratoChart.createChart(LibratoChartView(u, p, chartId, instrument.name))
+          }.map { chart =>
+            render.body(chart).header("Content-Type", "image/png")
           }
         }).getOrElse(render.status(BAD_REQUEST).toFuture)
       case _ => render.status(UNAUTHORIZED).toFuture
--- src/main/scala/laph/Templates.scala
+++ src/main/scala/laph/Templates.scala
@@ -3,9 +3,10 @@ package laph
 import com.twitter.finatra.View
 
 object Templates {
-  case class LibratoChartView(libratoUser: String,
+  case class LibratoChartView(libratoUser:  String,
                               libratoToken: String,
-                              chartId: Long) extends View {
+                              chartId:      Long,
+                              chartTitle:   String) extends View {
     val template = "librato-chart"
   }
 }
